81515e2b0848831047fb8a67b2d655caa8f9077e
Ankit-rajne <ankitrajne123@gmail.com>
2025-06-09 13:55:42 +0530

    Adding html css or javascript files

---
+++ script.js
@@ -0,0 +1,6 @@
+    var typed = new Typed("#element", {
+      strings: ["Web Developer", "Python Programmer", "Tech Enthusiast"],
+      typeSpeed: 60,
+      backSpeed: 40,
+      loop: true
+    });
